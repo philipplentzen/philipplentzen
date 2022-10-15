85953320ae6b9faf791d2deab21ca26b1bb34562
Philipp Lentzen <29258187+philipplentzen@users.noreply.github.com>
2022-10-15 21:21:53 +0200

    Fix my tailwind prefix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	prefix: `pl`,
+	prefix: `pl-`,
 	content: [
 		`./pages/**/*.{js,ts,jsx,tsx}`,
 		`./components/**/*.{js,ts,jsx,tsx}`

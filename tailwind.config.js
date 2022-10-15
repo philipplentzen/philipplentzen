478d4b45241b914fff4e507ea1e694009a5c74d8
Philipp Lentzen <29258187+philipplentzen@users.noreply.github.com>
2022-10-15 21:21:13 +0200

    Add my color palette to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	prefix: `pl`,
+	content: [
+		`./pages/**/*.{js,ts,jsx,tsx}`,
+		`./components/**/*.{js,ts,jsx,tsx}`
+	],
+	theme: {
+		extend: {
+			colors: {
+				white: `#F2F7FA`,
+				black: `#0F1E29`,
+				yellow: `#FCBA04`,
+				blue: `#1E3F56`,
+				cyan: `#18A999`
+			}
+		},
+	},
+	plugins: [],
+};

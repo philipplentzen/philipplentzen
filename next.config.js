eef60becad60b3ac3da1082ecc649b4e914e63a5
Philipp Lentzen <29258187+philipplentzen@users.noreply.github.com>
2022-10-26 13:09:29 +0200

    Adds locale tag

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
 	reactStrictMode: true,
 	output: `standalone`,
 	swcMinify: true,
+	i18n: {
+		locales: [`en`],
+		defaultLocale: `en`
+	},
 	webpack(config) {
 		config.module.rules.push({
 			test: /\.svg$/,
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from 'next';
-import Head from "next/head";
 import Logo from "../public/philipplentzen.svg";
 import {SocialLink} from "../components/Link";
 import {SEO} from "../components/SEO";

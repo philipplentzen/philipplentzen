fe1ecc0a3fece69e938daabd8715218890a9212a
Philipp Lentzen <29258187+philipplentzen@users.noreply.github.com>
2022-11-16 18:39:57 +0100

    Removes main from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,10 @@ import "../styles/globals.css";
 const Layout = ({children}: {children: React.ReactNode}) => {
 	return (
 		<html className={`pl-min-h-full pl-h-full pl-bg-white pl-text-black`} lang={`de`}>
-			<body className={`pl-h-full`}>
-				{children}
+			<body className={`pl-h-full pl-text-black pl-py-24 pl-px-6 lg:pl-px-0`}>
+				<main className={`pl-max-w-screen-lg pl-min-h-full pl-mx-auto`}>
+					{children}
+				</main>
 			</body>
 		</html>
 	);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,11 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import {Footer} from "../components/layout/Footer";
 import React from "react";
-import {Main} from "../components/layout/Main";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
-			<Main>
-				<Component {...pageProps} />
-			</Main>
+			<Component {...pageProps} />
 			<Footer />
 		</>
 	);

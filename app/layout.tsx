88880b2236cddd97c9327c81c587cb1045133ebb
Philipp Lentzen <29258187+philipplentzen@users.noreply.github.com>
2022-11-29 23:18:40 +0100

    Update root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,19 @@
 import "../styles/globals.css";
+import Link from "next/link";
+import React from "react";
 
 const Layout = ({children}: {children: React.ReactNode}) => {
 	return (
 		<html className={`pl-min-h-full pl-h-full pl-bg-white pl-text-black`} lang={`de`}>
-			<body className={`pl-h-full pl-text-black pl-py-24 pl-px-6 lg:pl-px-0`}>
-				<main className={`pl-max-w-screen-lg pl-min-h-full pl-mx-auto`}>
+			<body className={`pl-min-h-full pl-text-black pl-py-24 pl-px-6 lg:pl-px-0`}>
+				<main className={`pl-max-w-screen-lg pl-mx-auto pl-mt-12`}>
 					{children}
 				</main>
+				<footer className={`pl-max-w-screen-lg pl-mx-auto pl-mt-3 pl-text-center`}>
+					<Link href={`/impressum`} className={`pl-text-black/30 pl-mb-0 pl-text-xs`}>
+						impressum
+					</Link>
+				</footer>
 			</body>
 		</html>
 	);
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import {SocialLink} from "../components/Link";
 const Home = () => {
 	return (
 		<article className={`pl-h-full pl-flex pl-flex-col pl-justify-center pl-items-center`}>
-			<h3 className={`pl-text-sm pl-text-black/25`}>
+			<h3 className={`pl-text-sm pl-text-black/60`}>
 				Currently WIP
 			</h3>
 			<h1 className={`pl-w-11/12 md:pl-w-6/12 pl-leading-4  pl-mt-3`}>
@@ -13,10 +13,11 @@ const Home = () => {
 			<h2 className={`pl-text-xl md:pl-text-2xl pl-font-mono pl-tracking-tight pl-text-yellow pl-mt-6`}>
 				<b>&#47;&#47; TODO:</b> Start coding
 			</h2>
-			<aside className={`pl-w-4/12 sm:pl-w-2/12 pl-mx-auto pl-mt-12 pl-text-white`}>
+			<aside className={`pl-w-6/12 sm:pl-w-2/12 pl-mx-auto pl-mt-12 pl-text-white`}>
 				<div className={`pl-w-full pl-flex`}>
-					<SocialLink href={`https://github.com/philipplentzen`} target={`_blank`} icon={`github`} title={`GitHub`} className={`pl-w-1/2 !pl-bg-blue`} />
-					<SocialLink href={`https://www.linkedin.com/in/philipplentzen/`} target={`_blank`} icon={`linkedin`} title={`LinkedIn`} className={`pl-w-1/2 !pl-bg-cyan`} />
+					<SocialLink href={`https://github.com/philipplentzen`} target={`_blank`} icon={`github`} title={`GitHub`} className={`pl-w-1/3 !pl-bg-blue`} />
+					<SocialLink href={`https://www.linkedin.com/in/philipplentzen/`} target={`_blank`} icon={`linkedin`} title={`LinkedIn`} className={`pl-w-1/3 !pl-bg-cyan`} />
+					<SocialLink href={`mailto:kontakt@philipplentzen.dev`} target={`_blank`} icon={`mail`} title={`GitHub`} className={`pl-w-1/3`} />
 				</div>
 			</aside>
 		</article>

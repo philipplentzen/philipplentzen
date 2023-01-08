af1dc0f30addc2ccda04f10a32a613b512a409b7
Philipp Lentzen <29258187+philipplentzen@users.noreply.github.com>
2023-01-08 17:23:28 +0100

    Removes "currently WIP"

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,6 @@ const Layout = ({children}: {children: React.ReactNode}) => {
 		<html className={`pl-min-h-full pl-h-full pl-bg-white pl-text-black`} lang={`de`}>
 			<body className={`pl-min-h-full pl-text-black pl-py-24 pl-px-6 lg:pl-px-0`}>
 				<header className={`pl-max-w-screen-lg pl-mx-auto pl-flex pl-flex-col pl-items-center`}>
-					<h3 className={`pl-text-sm pl-text-black/60`}>
-						currently WIP
-					</h3>
 					<Link href={`/`} className={`pl-block pl-w-11/12 md:pl-w-6/12`}>
 						<h1 className={`pl-leading-4 pl-mt-3`}>
 							<Logo title={`Philipp Lentzen`} />

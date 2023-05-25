3c4f3171e3ecba2ba8ad0b4950ba0f36c05d799e
Philipp Lentzen <29258187+philipplentzen@users.noreply.github.com>
2023-05-25 20:19:00 +0200

    Makes landing page responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,16 +40,16 @@ export const metadata: Metadata = {
 
 const Header = () => {
 	return (
-		<header className={`pl-fixed pl-left-0 pl-right-0 pl-top-0 pl-z-50 pl-mx-auto pl-mt-12 xl:pl-mt-24 pl-w-screen pl-max-w-screen-xl pl-px-6 sm:pl-px-12 xl:pl-px-0`}>
+		<header className={`pl-fixed pl-inset-0 pl-z-50 pl-mx-auto pl-mt-12 xl:pl-mt-24 pl-w-screen pl-max-w-screen-xl pl-px-6 sm:pl-px-12 xl:pl-px-0`}>
 			<div className={`pl-flex pl-w-full pl-justify-between`}>
-				<Link href={`/`} className={`pl-block pl-h-0 pl-w-6/12 sm:pl-w-3/12 xl:pl-w-2/12 pl-pb-[4.5%] sm:pl-pb-[2.3%] xl:pl-pb-[1.5%]`}>
+				<Link href={`/`} className={`pl-block pl-h-0 pl-w-6/12 sm:pl-w-3/12 lg:pl-w-2/12 pl-pb-[4.5%] sm:pl-pb-[2.3%] lg:pl-pb-[1.5%]`}>
 					<h1 className={`pl-mt-[-4%]`}>
 						<Logo title={`Philipp Lentzen`} className={`pl-w-full`} />
 					</h1>
 				</Link>
 				<button className={`pl-block pl-flex pl-w-2/12 pl-flex-col pl-justify-between`}>
-					<div className={`pl-w-full pl-bg-black pl-pb-[6.6%] sm:pl-pb-[3.4%] xl:pl-pb-[2.4%]`}></div>
-					<div className={`pl-w-full pl-bg-black pl-pb-[6.6%] sm:pl-pb-[3.4%] xl:pl-pb-[2.4%]`}></div>
+					<div className={`pl-w-full pl-bg-black pl-pb-[6.6%] sm:pl-pb-[3.4%] lg:pl-pb-[2.4%]`}></div>
+					<div className={`pl-w-full pl-bg-black pl-pb-[6.6%] sm:pl-pb-[3.4%] lg:pl-pb-[2.4%]`}></div>
 				</button>
 			</div>
 		</header>
--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,16 @@ import LogoBig from "/public/philipplentzenbig.svg";
 const Home = () => {
 	return (
 		<>
-			<section className={`pl-relative pl-h-screen pl-flow-root pl-bg-gradient-to-b pl-from-cyan/20 pl-to-transparent`}>
-				<div className={`pl-relative pl-max-w-screen-xl pl-mx-auto pl-mt-[24rem] pl-px-6 lg:pl-px-0`}>
-					<div className={`pl-w-8/12 pl-mx-auto pl-h-24 pl-flow-root`}>
+			<section className={`pl-relative pl-h-screen supports-ios:pl-h-[100svh] pl-flow-root pl-bg-gradient-to-b pl-from-cyan/20 pl-to-transparent`}>
+				<div className={`pl-relative pl-max-w-screen-xl pl-mx-auto pl-mt-[21rem] lg:pl-mt-[18rem] xl:pl-mt-[24rem] pl-px-6 lg:pl-px-0`}>
+					<div className={`pl-w-10/12 lg:pl-w-8/12 pl-mx-auto pl-h-12 xl:pl-h-24 pl-flow-root`}>
 						<div className={`pl-mt-[-3.8%]`}>
 							<LogoBig className={``} />
 						</div>
 					</div>
 				</div>
-				<Image src={Clouds} alt={``} className={`pl-absolute pl-top-0`} />
-				<Image src={Grass} alt={``} className={`pl-absolute pl-bottom-0`} />
+				<Image src={Clouds} alt={``} className={`pl-absolute pl-top-12 xl:pl-top-0 pl-max-w-none pl-w-[200%] md:pl-w-full -pl-left-2/4 md:pl-left-0`} />
+				<Image src={Grass} alt={``} className={`pl-absolute pl-bottom-0 pl-max-w-none pl-w-[200%] md:pl-w-full -pl-left-2/4 md:pl-left-0`} />
 			</section>
 		</>
 	);

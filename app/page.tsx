a96799af3482c80cedf63b065212a9fd02acc6a9
Philipp Lentzen <29258187+philipplentzen@users.noreply.github.com>
2023-05-25 21:10:13 +0200

    Removes header for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,12 @@ const Home = () => {
 		<>
 			<section className={`pl-relative pl-h-screen supports-ios:pl-h-[100svh] pl-flow-root pl-bg-gradient-to-b pl-from-cyan/20 pl-to-transparent`}>
 				<div className={`pl-relative pl-max-w-screen-xl pl-mx-auto pl-mt-[18rem] lg:pl-mt-[15rem] xl:pl-mt-[24rem] pl-px-6 lg:pl-px-0`}>
-					<div className={`pl-w-10/12 lg:pl-w-8/12 pl-mx-auto pl-h-12 md:pl-h-24 pl-flow-root`}>
+					<div className={`pl-w-12/12 lg:pl-w-8/12 pl-mx-auto pl-h-12 md:pl-h-24 pl-flow-root`}>
 						<div className={`pl-mt-[-3.8%]`}>
 							<LogoBig className={``} />
 						</div>
 					</div>
-					<div className={`pl-w-4/12 sm:pl-w-3/12 lg:pl-w-2/12 pl-mx-auto pl-mt-6 lg:pl-mt-12 pl-flex`}>
+					<div className={`pl-w-4/12 sm:pl-w-3/12 lg:pl-w-2/12 pl-mx-auto pl-mt-3 lg:pl-mt-12 pl-flex`}>
 						<SocialLink href={`https://github.com/philipplentzen`} target={`_blank`} icon={`github`} title={`GitHub`} className={`pl-w-1/3 pl-pointer-events-auto`} />
 						<SocialLink href={`https://www.linkedin.com/in/philipplentzen/`} target={`_blank`} icon={`linkedin`} title={`LinkedIn`} className={`pl-w-1/3`} />
 						<SocialLink href={`mailto:kontakt@philipplentzen.dev`} target={`_blank`} icon={`mail`} title={`E-Mail`} className={`pl-w-1/3`} />

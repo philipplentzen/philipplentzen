ad2dbbf85c1e861a2bf1b84421c8e6f76d06a845
Philipp Lentzen <29258187+philipplentzen@users.noreply.github.com>
2023-06-01 14:07:22 +0200

    Adds ButtonLink component

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Grass from "/public/images/grass.png";
 import Clouds from "/public/images/clouds.png";
 import LogoBig from "/public/philipplentzenbig.svg";
-import {SocialLink} from "../components/Link";
+import {ButtonLink} from "../components/Links";
 
 const Home = () => {
 	return (
@@ -14,11 +14,9 @@ const Home = () => {
 							<LogoBig className={``} />
 						</div>
 					</div>
-					<div className={`pl-w-4/12 sm:pl-w-3/12 lg:pl-w-2/12 pl-mx-auto pl-mt-3 lg:pl-mt-12 pl-flex`}>
-						<SocialLink href={`https://github.com/philipplentzen`} target={`_blank`} icon={`github`} title={`GitHub`} className={`pl-w-1/3 pl-pointer-events-auto`} />
-						<SocialLink href={`https://www.linkedin.com/in/philipplentzen/`} target={`_blank`} icon={`linkedin`} title={`LinkedIn`} className={`pl-w-1/3`} />
-						<SocialLink href={`mailto:kontakt@philipplentzen.dev`} target={`_blank`} icon={`mail`} title={`E-Mail`} className={`pl-w-1/3`} />
-					</div>
+					<ButtonLink href={`#projects`} className={`pl-mx-auto pl-mt-3 lg:pl-mt-12`}>
+						.scrollToExplore()
+					</ButtonLink>
 				</div>
 				<Image src={Clouds} alt={``} className={`pl-absolute pl-top-12 xl:pl-top-0 pl-max-w-none pl-w-[200%] md:pl-w-full -pl-left-2/4 md:pl-left-0 pl-pointer-events-none`} />
 				<Image src={Grass} alt={``} className={`pl-absolute pl-bottom-0 pl-max-w-none pl-w-[200%] md:pl-w-full -pl-left-2/4 md:pl-left-0 pl-pointer-events-none`} />
--- components/Links.tsx
+++ components/Links.tsx
@@ -10,17 +10,24 @@ interface Props {
 	target?: HTMLAttributeAnchorTarget;
 }
 
-const GenericLink = forwardRef<HTMLAnchorElement, Props>(({children, className, href, target}, ref) => {
+const GenericLink = forwardRef<HTMLAnchorElement, Props>(({children, ...props}, ref) => {
 	return (
-		<Link href={href} ref={ref} target={target} className={classNames(`pl-block pl-bg-black pl-text-white pl-p-2 md:pl-p-3`, className)}>
+		<Link ref={ref} {...props}>
 			{children}
 		</Link>
 	);
 });
 GenericLink.displayName = `GenericLink`;
 
+export const ButtonLink = forwardRef<HTMLAnchorElement, Props>(({className, ...props}, ref) => {
+	return(
+		<GenericLink ref={ref} className={classNames(`pl-block pl-bg-black pl-text-white pl-height-12 pl-leading-[3rem] pl-font-mono pl-w-2/12 pl-text-center`, className)} {...props} />
+	);
+});
+ButtonLink.displayName = `ButtonLink`;
+
 export const SocialLink = forwardRef<HTMLAnchorElement, Omit<Props, `children`> & {icon: IconName, title: string}>(({className, icon, title, ...props}, ref) => (
-	<GenericLink className={classNames(`pl-aspect-square`, className)} ref={ref} {...props}>
+	<GenericLink className={classNames(`pl-bg-black pl-text-white pl-block pl-aspect-square pl-p-2 md:pl-p-3`, className)} ref={ref} {...props}>
 		<Icon name={icon} title={title} />
 	</GenericLink>
 ));

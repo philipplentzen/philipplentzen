0cbe3da2240f78fa971b136808f6113750a91908
Philipp Lentzen <29258187+philipplentzen@users.noreply.github.com>
2023-07-06 15:20:51 +0200

    Refactors classNames to custom cn function

--- components/content.tsx
+++ components/content.tsx
@@ -1,10 +1,10 @@
-import classNames from "classnames";
 import {ElementType, JSX} from "react";
+import {cn} from "@/lib/utils";
 
 type ContentProps = {as?: ElementType} & JSX.IntrinsicElements[`div`]
 export const Content = ({as: Comp = `div`, className, children}: ContentProps) => {
 	return (
-		<Comp className={classNames(`pl-relative pl-w-10/12 pl-mx-auto`, className)}>
+		<Comp className={cn(`pl-relative pl-w-10/12 pl-mx-auto`, className)}>
 			{children}
 		</Comp>
 	);
---
+++ components/ui/headings.tsx
@@ -0,0 +1,20 @@
+import {cn} from "@/lib/utils";
+import {ElementType, ReactNode} from "react";
+
+interface HeadingProps {
+    as: ElementType;
+    children: ReactNode;
+    className?: string;
+}
+
+const Heading = ({as: Comp, className, children}: HeadingProps) => {
+	return (
+		<Comp className={cn(`pl-relative`, className)}>
+			{children}
+		</Comp>
+	);
+};
+
+export const H1 = ({className, ...props}: Omit<HeadingProps, `as`>) => (
+	<Heading as={`h1`} className={cn(`pl-lowercase pl-text-9xl -pl-ml-2.5 pl-mt-[1.9rem] pl-pb-[2.1rem] pl-pointer-events-none`, className)} {...props} />
+);

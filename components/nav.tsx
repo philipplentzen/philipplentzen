7f559cf87fb25f2d0f908bb9409b3a57462ff2a1
Philipp Lentzen <29258187+philipplentzen@users.noreply.github.com>
2023-07-10 00:18:56 +0200

    Fixes build

--- components/nav.tsx
+++ components/nav.tsx
@@ -6,7 +6,6 @@ import { usePathname } from "next/navigation";
 import classNames from "classnames";
 
 import Logo from "../public/philipplentzen.svg";
-import { Content } from "./content";
 import { Section } from "./ui/section";
 
 type Link = {
@@ -31,34 +30,31 @@ export const Nav = () => {
   return (
     <>
       <Section
-        as={`nav`}
         className={`!pl-fixed pl-z-50 pl-h-screen pl-w-screen pl-bg-black pl-text-white`}
       >
-        <Content>
-          <ol
-            className={`pl-space-y-12 pl-font-mono pl-text-7xl pl-font-light pl-tracking-tighter`}
-            style={{ counterReset: `nav -1` }}
-          >
-            {links.map((link, index) => {
-              const isActive = pathname === link.href.toString();
-              console.log(pathname);
-              return (
-                <li
-                  key={index}
-                  className={classNames(
-                    `before:pl-content-[counter(nav)':'] before:pl-mr-2 before:pl-text-cyan before:pl-text-4xl`,
-                    { "pl-text-yellow": isActive }
-                  )}
-                  style={{ counterIncrement: `nav` }}
-                >
-                  <Link href={link.href}>
-                    {`{${link.name.toLowerCase()}: ${isActive}}`}
-                  </Link>
-                </li>
-              );
-            })}
-          </ol>
-        </Content>
+        <ol
+          className={`pl-space-y-12 pl-font-mono pl-text-7xl pl-font-light pl-tracking-tighter`}
+          style={{ counterReset: `nav -1` }}
+        >
+          {links.map((link, index) => {
+            const isActive = pathname === link.href.toString();
+            console.log(pathname);
+            return (
+              <li
+                key={index}
+                className={classNames(
+                  `before:pl-content-[counter(nav)':'] before:pl-mr-2 before:pl-text-cyan before:pl-text-4xl`,
+                  { "pl-text-yellow": isActive }
+                )}
+                style={{ counterIncrement: `nav` }}
+              >
+                <Link href={link.href}>
+                  {`{${link.name.toLowerCase()}: ${isActive}}`}
+                </Link>
+              </li>
+            );
+          })}
+        </ol>
         <div
           className={`pl-absolute pl-bottom-0 pl-left-0 pl-w-full pl-py-6 pl-text-right pl-font-mono`}
         >

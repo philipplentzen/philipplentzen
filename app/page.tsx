3cd63b7f9885be4acb8184dd34e5ca02b7d902bc
Philipp Lentzen <29258187+philipplentzen@users.noreply.github.com>
2023-08-13 00:32:41 +0200

    Revert "Remove projects section"
    This reverts commit 4a03c0099f3c451a57a36bd37ea9771d3df3838f.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import { buttonVariants } from "@/components/ui/buttons";
 import { Section } from "@/components/ui/section";
+import { H1, P } from "@/components/ui/typography";
 
 const IndexPage = () => {
   return (
@@ -23,6 +25,29 @@ const IndexPage = () => {
         <a href={`#projects`} className={buttonVariants()}>
           .scrollToExplore()
         </a>
+      </Section>
+      <Section id={`projects`}>
+        <H1>Projekte</H1>
+        <div className={`pl-grid pl-w-full pl-grid-cols-6 pl-gap-4`}>
+          <div
+            className={`pl-col-span-6 pl-h-48 pl-bg-cyan pl-shadow-inner xl:pl-col-span-3 xl:pl-h-72`}
+          ></div>
+          <div
+            className={`pl-col-span-6 pl-h-48 pl-bg-cyan pl-shadow-inner xl:pl-col-span-3 xl:pl-h-72`}
+          ></div>
+          <div
+            className={`pl-col-span-6 pl-h-48 pl-bg-cyan pl-shadow-inner xl:pl-col-span-4  xl:pl-h-72`}
+          ></div>
+          <div
+            className={`pl-col-span-6 pl-h-48 pl-bg-cyan pl-shadow-inner xl:pl-col-span-2 xl:pl-h-72`}
+          ></div>
+        </div>
+        <Link
+          href={`/`}
+          className={buttonVariants({ className: `pl-self-end` })}
+        >
+          .showMoreProjects()
+        </Link>
       </Section>
     </>
   );

feede2459609b1943a7c7e8e2470def9457ae8b8
Philipp Lentzen <29258187+philipplentzen@users.noreply.github.com>
2024-03-07 15:29:22 +0100

    Adds more details to projects list and prepares preview images

--- app/page.tsx
+++ app/page.tsx
@@ -53,31 +53,39 @@ export default function HomePage() {
 
       <Section id={`projects`}>
         <H1>Projekte</H1>
-        <List divided>
+        <List>
           {projects.map((project, index) => (
-            <ListItem
-              key={index}
-              className={`h-14 items-center space-x-6 font-mono transition-colors hover:text-cyan`}
-            >
-              <div className={`mb-px flex w-fit flex-none`}>
-                <Badge className={`font-normal`}>{project.year}</Badge>
+            <ListItem key={index} className={`grid gap-6 py-6 lg:grid-cols-2`}>
+              <div className={`h-72 bg-cyan/10 ring-1 ring-cyan`}></div>
+              <div className={`flex-col items-center space-y-3`}>
+                <div className={`flex h-9 items-center space-x-6`}>
+                  <div className={`mb-px flex w-fit flex-none`}>
+                    <Badge className={`font-normal`}>{project.year}</Badge>
+                  </div>
+                  <H2 className={`m-0 w-full font-mono text-xl`}>
+                    {project.title}
+                  </H2>
+                  {project.url && (
+                    <a
+                      href={project.url}
+                      target={`_blank`}
+                      className={buttonVariants({
+                        variant: `ghost`,
+                        size: `square`,
+                        className: `flex-none`,
+                      })}
+                    >
+                      <ArrowUpRightIcon size={20} />
+                    </a>
+                  )}
+                </div>
+                <P className={`pr-12`}>
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                  diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                  aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                  justo duo dolores et ea rebum.
+                </P>
               </div>
-              <H2 className={`m-0 w-fit flex-none text-xl`}>{project.title}</H2>
-              <P className={`!mt-0 w-full text-sm`}></P>
-              {project.url && (
-                <a
-                  href={project.url}
-                  aria-label={`${project.title} öffnen`}
-                  target={`_blank`}
-                  className={buttonVariants({
-                    variant: `ghost`,
-                    size: `square`,
-                    className: `flex-none`,
-                  })}
-                >
-                  <ArrowUpRightIcon size={20} />
-                </a>
-              )}
             </ListItem>
           ))}
         </List>

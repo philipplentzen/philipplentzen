718393a8f1a138d99a9d5b604da66c27e7187faa
Philipp Lentzen <29258187+philipplentzen@users.noreply.github.com>
2024-03-07 14:41:24 +0100

    Clean up

--- app/page.tsx
+++ app/page.tsx
@@ -11,18 +11,6 @@ import { Logo } from "@/components/logo";
 
 export default function IndexPage() {
   const projects = sortBy(allProjects, [`year`]).reverse();
-  const sections = [
-    {
-      id: `projects`,
-      title: `Projekte`,
-      items: projects.map((project) => ({
-        badge: project.year,
-        title: project.title,
-        description: ``,
-        url: project.url,
-      })),
-    },
-  ];
 
   return (
     <>
@@ -50,39 +38,37 @@ export default function IndexPage() {
         </div>
       </Section>
 
-      {sections.map((section) => (
-        <Section id={section.id} key={section.id}>
-          <H1>{section.title}</H1>
-          <List divided>
-            {section.items.map((item, index) => (
-              <ListItem
-                key={index}
-                className={`h-14 items-center space-x-6 font-mono transition-colors hover:text-cyan`}
-              >
-                <div className={`mb-px flex w-fit flex-none`}>
-                  <Badge className={`font-normal`}>{item.badge}</Badge>
-                </div>
-                <H2 className={`m-0 w-fit flex-none text-xl`}>{item.title}</H2>
-                <P className={`!mt-0 w-full text-sm`}>{item.description}</P>
-                {item.url && (
-                  <a
-                    href={item.url}
-                    aria-label={`${item.title} öffnen`}
-                    target={`_blank`}
-                    className={buttonVariants({
-                      variant: `ghost`,
-                      size: `square`,
-                      className: `flex-none`,
-                    })}
-                  >
-                    <ArrowUpRightIcon size={20} />
-                  </a>
-                )}
-              </ListItem>
-            ))}
-          </List>
-        </Section>
-      ))}
+      <Section id={`projects`}>
+        <H1>Projekte</H1>
+        <List divided>
+          {projects.map((project, index) => (
+            <ListItem
+              key={index}
+              className={`h-14 items-center space-x-6 font-mono transition-colors hover:text-cyan`}
+            >
+              <div className={`mb-px flex w-fit flex-none`}>
+                <Badge className={`font-normal`}>{project.year}</Badge>
+              </div>
+              <H2 className={`m-0 w-fit flex-none text-xl`}>{project.title}</H2>
+              <P className={`!mt-0 w-full text-sm`}></P>
+              {project.url && (
+                <a
+                  href={project.url}
+                  aria-label={`${project.title} öffnen`}
+                  target={`_blank`}
+                  className={buttonVariants({
+                    variant: `ghost`,
+                    size: `square`,
+                    className: `flex-none`,
+                  })}
+                >
+                  <ArrowUpRightIcon size={20} />
+                </a>
+              )}
+            </ListItem>
+          ))}
+        </List>
+      </Section>
     </>
   );
 }

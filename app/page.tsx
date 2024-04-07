cfb76d035839ae98fddae42955d67aba207e3448
Philipp Lentzen <29258187+philipplentzen@users.noreply.github.com>
2024-04-07 13:33:51 +0200

    Adds ellipses to end of text

--- app/page.tsx
+++ app/page.tsx
@@ -88,7 +88,7 @@ export default function HomePage() {
                   Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
                   diam nonumy eirmod tempor invidunt ut labore et dolore magna
                   aliquyam erat, sed diam voluptua. At vero eos et accusam et
-                  justo duo dolores et ea rebum.
+                  justo duo dolores et ea rebum...
                 </P>
               </div>
             </ListItem>

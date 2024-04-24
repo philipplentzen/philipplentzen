32431ea266c71f3c9edd971d9336e70a6db84b7d
Philipp Lentzen <code@philipplentzen.dev>
2024-04-24 17:40:47 +0200

    Updates opengraph image

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -2,7 +2,7 @@ import { ImageResponse } from "next/og";
 
 import { Logo } from "@/components/logo";
 
-export const alt = `kreativer entwickler. informatik student. aus der kaiserstadt.`;
+export const alt = `kreativer entwickler. informatik student. öcher jong.`;
 export const size = {
   width: 1200,
   height: 630,
@@ -18,35 +18,73 @@ export default async function Image() {
           display: `flex`,
           flexDirection: `column`,
           justifyContent: `center`,
-          alignItems: `center`,
+          alignItems: `flex-start`,
           width: `100%`,
           height: `100%`,
           backgroundColor: `#F2F7FA`,
           backgroundImage: `url('https://philipplentzen.dev/images/grain.png'), linear-gradient(to bottom, rgba(24,169, 153, 0.3), rgba(24,169, 153, 0))`,
-          gap: `1rem`,
-          fontSize: `1.2rem`,
+          gap: `0.5rem`,
+          padding: `6rem`,
         }}
       >
         <img
           src={`https://philipplentzen.dev/images/clouds.png`}
           style={{ position: `absolute` }}
         />
-        <span style={{ color: `#18A999`, fontFamily: `ui-monospace` }}>
-          kreativer entwickler. informatik student. aus der kaiserstadt.
-        </span>
+        <div
+          style={{
+            display: `flex`,
+            alignItems: `center`,
+            color: `rgba(15, 30, 41, 0.8)`,
+          }}
+        >
+          <span style={{ position: `relative` }}>
+            <span
+              style={{
+                left: `-0.25rem`,
+                top: `-0.25rem`,
+                position: `absolute`,
+                borderRadius: `100%`,
+                width: `1.25rem`,
+                height: `1.25rem`,
+                backgroundColor: `#18A999`,
+                marginRight: `0.5rem`,
+                opacity: 0.25,
+              }}
+            ></span>
+            <span
+              style={{
+                borderRadius: `100%`,
+                width: `0.75rem`,
+                height: `0.75rem`,
+                backgroundColor: `#18A999`,
+                marginRight: `0.5rem`,
+              }}
+            ></span>
+          </span>
+          verfügbar
+        </div>
         <Logo />
         <div
           style={{
             display: `flex`,
-            color: `rgba(255,255,255,0.75)`,
-            backgroundColor: `#000`,
-            padding: `1rem 2rem`,
-            marginTop: `2rem`,
+            flexDirection: `column`,
           }}
         >
-          <span>kontakt</span>
-          <span style={{ color: `#FFF` }}>@philipplentzen</span>
-          <span>.dev</span>
+          {[`Kreativer Entwickler`, `Informatikstudent`, `Öcher Jong`].map(
+            (text) => (
+              <span
+                key={text}
+                style={{
+                  color: `rgba(15, 30, 41, 0.6)`,
+                  fontSize: `2rem`,
+                  lineHeight: 1,
+                }}
+              >
+                {text.toLowerCase()}.
+              </span>
+            )
+          )}
         </div>
       </div>
     ),

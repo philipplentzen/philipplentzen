9d0ff7e59c4a756fe4dc7e0dc14791eb0a896042
Philipp Lentzen <code@philipplentzen.dev>
2024-04-24 15:35:49 +0200

    Implements smaller content area

--- components/ui/section.tsx
+++ components/ui/section.tsx
@@ -11,10 +11,7 @@ export const Section = forwardRef<HTMLElement, ContainerProps>((props, ref) => {
   return (
     <section
       className={cn(
-        [
-          `group/section relative mx-auto mb-24 flex max-w-screen-md scroll-m-12 flex-col items-start space-y-6 border-t border-t-black/20 px-6 pt-6 max-sm:mb-10 lg:px-0`,
-          `first-of-type:my-72 first-of-type:border-t-0 first-of-type:pt-0 max-md:first-of-type:mb-24`,
-        ],
+        `group/section relative mx-auto flex max-w-screen-md scroll-m-12 flex-col items-start gap-6 border-t border-t-black/20 px-6 pb-30 pt-6 first-of-type:border-t-0 first-of-type:pt-96 lg:px-0`,
         className
       )}
       ref={ref}

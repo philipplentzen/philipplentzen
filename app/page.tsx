19804f5827ebec28be6ff6ecc02dd770e104d786
Philipp Lentzen <code@philipplentzen.dev>
2024-04-24 16:03:43 +0200

    Updates positioning and content of landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
 import { allProjects } from "contentlayer/generated";
 import { sortBy } from "lodash";
-import { ArrowUpRightIcon } from "lucide-react";
 
-import { Badge } from "@/components/ui/badge";
-import { buttonVariants } from "@/components/ui/buttons";
-import { List, ListItem } from "@/components/ui/list";
 import { Section } from "@/components/ui/section";
-import { H1, H2, H3, P } from "@/components/ui/typography";
+import { H1, H2 } from "@/components/ui/typography";
 import { Logo } from "@/components/logo";
 
 export default function HomePage() {
@@ -14,73 +10,45 @@ export default function HomePage() {
 
   return (
     <>
-      <Section>
-        <div className={`mt-0 sm:mt-12 xl:mt-20`}>
+      <Section className={`pb-72`}>
+        <Logo className={`mb-3 w-full drop-shadow sm:w-8/12 lg:w-6/12`} />
+        <div className={`-mt-8`}>
           <H2
-            className={`mx-auto w-1/2 space-x-1 space-y-1 text-center font-mono text-xs font-normal text-cyan sm:w-full`}
+            className={`space-y-2 font-normal text-black/60 after:content-none sm:w-full`}
           >
-            {[
-              `kreativer entwickler`,
-              `informatik student`,
-              `aus der kaiserstadt`,
-            ].map((text, index) => (
-              <span key={index} className={`inline-block whitespace-nowrap`}>
-                {text}.
-              </span>
-            ))}
+            {[`Kreativer Entwickler`, `Informatikstudent`, `Öcher Jong`].map(
+              (text, index) => (
+                <span key={index} className={`block`}>
+                  {text}.
+                </span>
+              )
+            )}
           </H2>
-        </div>
-        <Logo className={`-mt-3 mb-3 w-full drop-shadow sm:w-8/12 lg:w-6/12`} />
-        <div className={`mb-12 flex items-center space-x-6 sm:mb-30`}>
-          <a
-            href={`mailto:kontakt@philipplentzen.dev`}
-            aria-label={`E-Mail an mich schreiben`}
-            className={buttonVariants()}
-          >
-            .sayHello()
-          </a>
-          <H3
-            className={`flex items-center font-mono text-sm font-normal lowercase text-neutral-700`}
-          >
-            <span className="relative mr-2 flex size-3">
-              <span className="absolute inline-flex size-full animate-ping rounded-full bg-cyan opacity-75"></span>
-              <span className="relative inline-flex size-3 rounded-full bg-cyan"></span>
-            </span>
-            verfügbar
-          </H3>
         </div>
       </Section>
 
       <Section id={`projects`}>
-        <H1>Projekte</H1>
-        <List divided>
-          {projects.map((project, index) => (
-            <ListItem
-              key={index}
-              className={`h-14 items-center space-x-6 font-mono transition-colors hover:text-cyan`}
-            >
-              <div className={`mb-px flex w-fit flex-none`}>
-                <Badge className={`font-normal`}>{project.year}</Badge>
-              </div>
-              <H2 className={`m-0 w-fit flex-none text-xl`}>{project.title}</H2>
-              <P className={`!mt-0 w-full text-sm`}></P>
-              {project.url && (
-                <a
-                  href={project.url}
-                  aria-label={`${project.title} öffnen`}
-                  target={`_blank`}
-                  className={buttonVariants({
-                    variant: `ghost`,
-                    size: `square`,
-                    className: `flex-none`,
-                  })}
-                >
-                  <ArrowUpRightIcon size={20} />
-                </a>
-              )}
-            </ListItem>
+        <H1>Willkommen</H1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        <div
+          className={`mt-24 grid w-full grid-cols-2 rounded-xl border border-cyan bg-cyan/10 p-6`}
+        >
+          {[
+            { title: `Informationen`, items: [`Über mich`, `Kontakt`] },
+            { title: `Stöbern`, items: [`Projekte`] },
+          ].map(({ title, items }, i) => (
+            <div key={i}>
+              <H2>{title}</H2>
+              <ul className={`mt-6 space-y-3 pl-3 text-black/60`}>
+                {items.map((item, i) => (
+                  <li key={i} className={`transition-colors hover:text-cyan`}>
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </div>
           ))}
-        </List>
+        </div>
       </Section>
     </>
   );

f0977ddf7b2b7e2cd9a6897ebe296c9628ac5430
Philipp Lentzen <code@philipplentzen.dev>
2024-04-24 23:35:47 +0200

    Improves contact page

--- components/ui/section.tsx
+++ components/ui/section.tsx
@@ -12,7 +12,7 @@ export const Section = forwardRef<HTMLElement, ContainerProps>((props, ref) => {
     <section
       className={cn(
         [
-          `group/section relative mx-auto flex max-w-screen-md scroll-m-12 flex-col items-start gap-6 border-t border-t-black/20 px-6 pb-24 pt-6 max-sm:pb-10 lg:px-0`,
+          `group/section relative mx-auto flex max-w-screen-md scroll-m-12 flex-col items-start space-y-6 border-t border-t-black/20 px-6 pb-24 pt-6 max-sm:pb-10 lg:px-0`,
           `first-of-type:border-t-0 first-of-type:py-72 max-md:first-of-type:pb-24`,
         ],
         className

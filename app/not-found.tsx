e3acdcf42c0c41c0fcb27272042744cea6eb85f0
Philipp Lentzen <code@philipplentzen.dev>
2025-01-23 11:14:32 +0100

    Updates not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,17 @@
-import { H1 } from "@/components/ui/typography";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { Section } from "@/components/ui/section";
+import { H1, H2 } from "@/components/ui/typography";
 
 export default function NotFound() {
   return (
-    <>
+    <Section>
       <H1>404</H1>
-    </>
+      <H2 className={`after:content-['?']`}>Hast du dich verlaufen</H2>
+      <Button variant={`ghost`} asChild>
+        <Link href={`/`}>Zur Startseite</Link>
+      </Button>
+    </Section>
   );
 }

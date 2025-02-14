65b7601372b459dc637d8e6a42163575ef4c3caf
Philipp Lentzen <code@philipplentzen.dev>
2025-02-14 22:01:22 +0100

    Updates landing page to old content

---
+++ actions/projects.ts
@@ -0,0 +1,47 @@
+import fs from "fs";
+import process from "node:process";
+import path from "path";
+import _ from "lodash";
+
+export type Project = {
+  title: string;
+  year: number;
+  url?: string;
+  description: string;
+  image?: {
+    src: string;
+    width: number;
+    height: number;
+  };
+  task: string;
+  tags: string[];
+  employment: string;
+};
+
+export const getProjects = async (
+  image: string,
+  sortBy: (keyof Project)[]
+): Promise<Project[]> => {
+  const dir = path.join(process.cwd(), `app`, `projects`);
+  const files = await fs.promises.readdir(path.resolve(dir), {
+    recursive: true,
+  });
+  const mdxFilePaths = files.filter((file) => file.endsWith(`.mdx`));
+  const mdxFiles = await Promise.all(
+    mdxFilePaths.map(async (filePath) => import(`@/app/projects/${filePath}`))
+  );
+  const projects = mdxFiles.map(
+    ({ meta }: { meta: Project & { images: string[] } }, index) => {
+      const imagePath = meta.images!.includes(image)
+        ? `/images/projects/${mdxFilePaths[index].replace(`page.mdx`, `${image}.png`)}`
+        : undefined;
+
+      return {
+        ...meta,
+        image: imagePath,
+      };
+    }
+  );
+
+  return _.sortBy(projects, sortBy).reverse();
+};
---
+++ components/ui/image.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { ComponentProps, useState } from "react";
+import NextImage from "next/image";
+
+import { cn } from "@/lib/utils";
+import { Loading } from "@/components/ui/loading";
+
+export const Image = (props: ComponentProps<typeof NextImage>) => {
+  const { alt, className, ...otherProps } = props;
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  return (
+    <div
+      className={cn(
+        `relative border border-cyan/30 bg-cyan/20 text-cyan`,
+        className
+      )}
+    >
+      {!isLoaded && <Loading />}
+      <NextImage alt={alt} {...otherProps} onLoad={() => setIsLoaded(true)} />
+    </div>
+  );
+};

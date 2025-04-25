9c2aa62909858f5795b162b4c83723eed54d597f
Philipp Lentzen <code@philipplentzen.dev>
2025-04-25 17:08:48 +0200

    Remove --turbo flag

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,8 @@
 import createMDX from "@next/mdx";
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
   pageExtensions: [`js`, `jsx`, `ts`, `tsx`, `md`, `mdx`],
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: `@svgr/webpack`,
-          options: {
-            dimensions: false,
-            titleProp: true,
-          },
-        },
-      ],
-    });
-    return config;
-  },
 };
 
 const withMDX = createMDX({});

0398a3b14e115605ab033d5fcb392d15a4eeaabb
Philipp Lentzen <code@philipplentzen.dev>
2025-05-12 17:27:02 +0200

    Inits basic layout

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import createMDX from "@next/mdx";
-import type {NextConfig} from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   pageExtensions: [`js`, `jsx`, `ts`, `tsx`, `md`, `mdx`],
+  experimental: {
+    mdxRs: true,
+  },
 };
 
-const withMDX = createMDX({});
+const withMDX = createMDX();
 
 export default withMDX(nextConfig);
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,6 @@ const tailwindConfig = {
       backgroundImage: {
         grain: `url('/images/grain.png'), linear-gradient(to bottom, rgba(24,169, 153, 0.3), rgba(24,169, 153, 0.3))`,
       },
-      fontFamily: {
-        sans: [`var(--font-inter)`, ...fontFamily.sans],
-        overpass: [`var(--font-overpass)`, ...fontFamily.sans],
-      },
       typography: {
         DEFAULT: {
           css: {
@@ -22,18 +18,6 @@ const tailwindConfig = {
           },
         },
       },
-    },
-    fontSize: {
-      xs: [`min(0.75rem, 2.75vw)`, { lineHeight: 1 }],
-      sm: [`min(0.875rem, 2.75vw)`, { lineHeight: 1 }],
-      lg: [`min(1.125rem, 5vw)`, { lineHeight: 1 }],
-      xl: [`min(1.25rem, 5vw)`, { lineHeight: 1 }],
-      "2xl": [`min(1.5rem, 4vw)`, { lineHeight: 1 }],
-      "3xl": [`min(1.875rem, 5vw)`, { lineHeight: 1 }],
-      "4xl": [`min(2.25rem, 6vw)`, { lineHeight: 1 }],
-      "5xl": [`min(2.75rem, 7vw)`, { lineHeight: 1 }],
-      "6xl": [`min(3.75rem, 15vw)`, { lineHeight: 1 }],
-      "7xl": [`min(4.5rem, 12vw)`, { lineHeight: 1 }],
     },
     dropShadow: {
       DEFAULT: `6px 6px 6px rgba(15, 30, 41, 0.25)`,

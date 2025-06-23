345ce8904b5880adc9353a27ef5aa62cc4fd5813
Philipp Lentzen <code@philipplentzen.dev>
2025-06-24 01:01:27 +0200

    feat: add breakpoint debugger

--- app/layout.tsx
+++ app/layout.tsx
@@ -97,7 +97,16 @@ export default async function RootLayout({ children }: PropsWithChildren<{}>) {
       )}
       lang={`de`}
     >
-      <body className={`max-w-screen overflow-x-hidden`}>
+      <body className={`max-w-screen relative overflow-x-hidden`}>
+        {process.env.NODE_ENV === "development" && (
+          <div className={"fixed right-0 top-0 z-50 text-xs leading-none"}>
+            <span className={"hidden max-sm:inline-block"}>none</span>
+            <span className={"hidden sm:max-md:inline-block"}>sm</span>
+            <span className={"hidden md:max-lg:inline-block"}>md</span>
+            <span className={"hidden lg:max-xl:inline-block"}>lg</span>
+            <span className={"hidden xl:max-2xl:inline-block"}>xl</span>
+          </div>
+        )}
         <div
           className={cn(
             "container isolate mx-auto grid w-full",
@@ -131,7 +140,7 @@ export default async function RootLayout({ children }: PropsWithChildren<{}>) {
           </header>
           <main
             className={cn(
-              "@container relative flex flex-col gap-y-8 p-4 max-sm:overflow-x-hidden sm:gap-y-16 md:pl-[calc(var(--spacing)_*_8_-_1px)] md:pr-[calc(var(--spacing)_*_8_-_3px)]",
+              "@container relative flex flex-col gap-y-8 p-4 max-sm:overflow-x-hidden sm:gap-y-16 sm:pl-[calc(var(--spacing)_*_8_-_1px)] sm:pr-[calc(var(--spacing)_*_8_-_3px)]",
               "bg-position-[-1px_1px] bg-[url(/textures/grid-background.png)]",
               "border-black/20 sm:border-x-[2px]",
               "before:absolute before:left-[-100vw] before:top-0 before:h-[2px] before:w-[200vw] before:bg-black/20",

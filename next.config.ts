e17a6fa8a978165ec29ca7754d21a00dd81b3137
Philipp Lentzen <code@philipplentzen.dev>
2025-06-29 21:54:31 +0200

    chore: remove webpack config

--- next.config.ts
+++ next.config.ts
@@ -3,21 +3,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   pageExtensions: [`js`, `jsx`, `ts`, `tsx`, `md`, `mdx`],
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: `@svgr/webpack`,
-          options: {
-            dimensions: false,
-            titleProp: true,
-          },
-        },
-      ],
-    });
-    return config;
-  },
 };
 
 const withMDX = createMDX({});

4abf322afc1300088665113ea38f784d9cf8ed3f
Philipp Lentzen <code@philipplentzen.dev>
2025-06-30 00:38:23 +0200

    feat: add viewport border pattern

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Logo } from "@/components/logo";
 import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 import {
@@ -7,6 +8,7 @@ import {
   Inter,
   Overpass,
 } from "next/font/google";
+import Link from "next/link";
 import { PropsWithChildren } from "react";
 
 const caveat = Caveat({
@@ -60,9 +62,49 @@ export default function RootLayout({ children }: PropsWithChildren<{}>) {
             <span className={"hidden 2xl:inline-block"}>2xl</span>
           </div>
         )}
-        <div className={"container isolate mx-auto w-full"}>
-          <header></header>
-          <main>{children}</main>
+        <div
+          className={cn(
+            "container isolate mx-auto grid w-full [--divider-height:calc(var(--spacing)_*_4)] [--header-height:calc(var(--spacing)_*_16)] [--pattern-size:calc(var(--spacing)_*_2)]",
+            "grid-rows-[var(--header-height)_var(--divider-height)_minmax(calc(100dvh_-_(var(--header-height)_+_var(--divider-height))),1fr)_var(--divider-height)_auto]",
+            "grid-cols-1 [--edge-width:calc(6rem_+_1px)] md:grid-cols-[var(--edge-width)_minmax(0,1fr)_var(--edge-width)]",
+          )}
+        >
+          <div
+            className={cn(
+              "border-x-current/10 col-start-1 row-span-full hidden border-r md:block",
+              "bg-radial bg-size-[var(--pattern-size)_var(--pattern-size)] to-current/0 from-current/10 from-[1px] to-[1px]",
+              "mask-l-from-10%",
+            )}
+          />
+          <div
+            className={cn(
+              "border-x-current/10 col-start-3 row-span-full hidden border-l md:block",
+              "bg-radial bg-size-[var(--pattern-size)_var(--pattern-size)] to-current/0 from-current/10 from-[1px] to-[1px]",
+              "mask-r-from-10%",
+            )}
+          />
+          <header className={"flex items-center px-4"}>
+            <Link href={"/"} aria-label={"Startseite"} className={"-ml-2 p-2"}>
+              <Logo className={"h-8"} />
+            </Link>
+          </header>
+          <div
+            className={cn(
+              "relative",
+              "bg-radial bg-size-[var(--pattern-size)_var(--pattern-size)] to-current/0 from-current/10 from-[1px] to-[1px]",
+              "before:bg-current/10 before:absolute before:left-[-100vw] before:top-0 before:h-px before:w-[200vw]",
+              "after:bg-current/10 after:absolute after:bottom-0 after:left-[-100vw] after:h-px after:w-[200vw]",
+            )}
+          />
+          <main className={cn("@container relative")}>{children}</main>
+          <div
+            className={cn(
+              "relative",
+              "bg-radial bg-size-[var(--pattern-size)_var(--pattern-size)] to-current/0 from-current/10 from-[1px] to-[1px]",
+              "before:bg-current/10 before:absolute before:left-[-100vw] before:top-0 before:h-px before:w-[200vw]",
+              "after:bg-current/10 after:absolute after:bottom-0 after:left-[-100vw] after:h-px after:w-[200vw]",
+            )}
+          />
           <footer></footer>
         </div>
       </body>

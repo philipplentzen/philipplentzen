4e9c01e73df1e52f3a45ffdc8e9bfae3b36cded1
Philipp Lentzen <code@philipplentzen.dev>
2025-07-27 16:33:05 +0200

    feat: cleanup ui and reinterpret design language (#57)

---
+++ components/copy-button.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { CheckIcon, CopyIcon } from "lucide-react";
+import {
+  ComponentPropsWithoutRef,
+  ComponentRef,
+  forwardRef,
+  useCallback,
+  useState,
+} from "react";
+
+export const CopyButton = forwardRef<
+  ComponentRef<typeof Button>,
+  Omit<
+    ComponentPropsWithoutRef<typeof Button>,
+    "children" | "size" | "onClick"
+  > & {
+    value: string;
+  }
+>((props, ref) => {
+  const { variant = "ghost", value, className, ...otherProps } = props;
+  const [success, setSuccess] = useState(false);
+
+  const handleCopy = useCallback(async () => {
+    try {
+      await navigator.clipboard.writeText(value);
+      setSuccess(true);
+    } catch {
+      console.error("Failed to copy text to clipboard");
+    }
+  }, [setSuccess, value]);
+
+  return (
+    <Button
+      ref={ref}
+      variant={variant}
+      size={"square"}
+      onClick={handleCopy}
+      className={cn(success && "bg-current/20 text-secondary", className)}
+      {...otherProps}
+    >
+      {success ?
+        <CheckIcon />
+      : <CopyIcon />}
+    </Button>
+  );
+});
+CopyButton.displayName = "CopyButton";
---
+++ components/today.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import {
+  ComponentPropsWithoutRef,
+  forwardRef,
+  useEffect,
+  useState,
+} from "react";
+
+export const Today = forwardRef<
+  HTMLSpanElement,
+  ComponentPropsWithoutRef<"span">
+>((props, ref) => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  return (
+    <span ref={ref} {...props}>
+      {isClient && new Date().toLocaleDateString()}
+    </span>
+  );
+});
+Today.displayName = "Today";

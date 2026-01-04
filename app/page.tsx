370390fe7315ef55daff69102064ad3d29d76d83
Philipp Lentzen <philipp.lentzen@quantco.com>
2026-01-04 15:08:19 +0100

    feat: extract cta to reusable component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,11 @@
-import { sortBy } from "lodash";
-import {
-  GithubIcon,
-  InboxIcon,
-  LinkedinIcon,
-  SparklesIcon,
-} from "lucide-react";
+import { GithubIcon, InboxIcon, LinkedinIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { CopyButton } from "@/components/copy-button";
 import { ProjectList } from "@/components/project-list";
-import { Button } from "@/components/ui/button";
 import { Section } from "@/components/ui/section";
 import { Article, H2, H3 } from "@/components/ui/typography";
+import { YourNewWebsite } from "@/components/your-new-website";
 import HowIWork from "@/content/about-me/how-i-work.mdx";
 import WhatIDo from "@/content/about-me/what-i-do.mdx";
 import WhoIAm from "@/content/about-me/who-i-am.mdx";
@@ -63,60 +57,7 @@ export default async function HomePage() {
       </Section>
 
       <Section>
-        <div
-          className={
-            "relative grid w-full gap-x-3 rounded border border-text/20 bg-radial-[at_10%_10%] from-secondary/30 to-accent/10 lg:grid-cols-3"
-          }
-        >
-          <div
-            className={
-              "flex size-64 items-center justify-center justify-self-center border-text/20 p-(--padding-width) lg:size-full lg:border-r"
-            }
-          >
-            <Image
-              className={"object-contain drop-shadow-2xl"}
-              src={"/images/your-new-website.png"}
-              alt={"Alter PC mit einem Fragezeichen auf dem Bildschirm"}
-              width={294}
-              height={280}
-            />
-          </div>
-
-          <div
-            className={
-              "flex size-full flex-col justify-center border-text/20 p-(--padding-width) text-primary max-lg:border-t lg:col-span-2"
-            }
-          >
-            <Article className={"w-full"}>
-              <H2
-                className={cn(
-                  "text-[min(var(--text-7xl),_14cqw)] text-accent leading-[0.833]",
-                )}
-              >
-                <i>Deine neue Webseite?</i>
-              </H2>
-              <p className={"mt-4"}>
-                Du hast &apos;ne Idee im Kopf, ich bring sie ins Web. <br />
-                Mit Code, der sitzt. Mit Design, das wirkt. <br />
-                Und mit Spaß an der Sache.
-              </p>
-              <Button
-                asChild
-                variant={"outline"}
-                className={"text-secondary sm:mt-4"}
-              >
-                <Link
-                  href={"mailto:kontakt@philipplentzen.dev"}
-                  title={"Kontakt aufnehmen"}
-                  aria-label={"Kontakt aufnehmen"}
-                >
-                  <SparklesIcon />
-                  Lass uns loslegen!
-                </Link>
-              </Button>
-            </Article>
-          </div>
-        </div>
+        <YourNewWebsite />
       </Section>
 
       <Section>
---
+++ components/your-new-website.tsx
@@ -0,0 +1,65 @@
+import { SparklesIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+import { Article, H2 } from "./ui/typography";
+
+export const YourNewWebsite = () => {
+  return (
+    <div
+      className={
+        "relative grid w-full gap-x-3 rounded border border-text/20 bg-radial-[at_10%_10%] from-secondary/30 to-accent/10 lg:grid-cols-3"
+      }
+    >
+      <div
+        className={
+          "flex size-64 items-center justify-center justify-self-center border-text/20 p-(--padding-width) lg:size-full lg:border-r"
+        }
+      >
+        <Image
+          className={"object-contain drop-shadow-2xl"}
+          src={"/images/your-new-website.png"}
+          alt={"Alter PC mit einem Fragezeichen auf dem Bildschirm"}
+          width={294}
+          height={280}
+        />
+      </div>
+
+      <div
+        className={
+          "flex size-full flex-col justify-center border-text/20 p-(--padding-width) text-primary max-lg:border-t lg:col-span-2"
+        }
+      >
+        <Article className={"w-full"}>
+          <H2
+            className={cn(
+              "text-[min(var(--text-7xl),_14cqw)] text-accent leading-[0.833]",
+            )}
+          >
+            <i>Deine neue Webseite?</i>
+          </H2>
+          <p className={"mt-4"}>
+            Du hast &apos;ne Idee im Kopf, ich bring sie ins Web. <br />
+            Mit Code, der sitzt. Mit Design, das wirkt. <br />
+            Und mit Spaß an der Sache.
+          </p>
+          <Button
+            asChild
+            variant={"outline"}
+            className={"text-secondary sm:mt-4"}
+          >
+            <Link
+              href={"mailto:kontakt@philipplentzen.dev"}
+              title={"Kontakt aufnehmen"}
+              aria-label={"Kontakt aufnehmen"}
+            >
+              <SparklesIcon />
+              Lass uns loslegen!
+            </Link>
+          </Button>
+        </Article>
+      </div>
+    </div>
+  );
+};

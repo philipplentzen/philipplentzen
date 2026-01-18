df08189619fbb5048fab624ea3552a0a14ad203c
Philipp Lentzen <code@philipplentzen.dev>
2026-01-19 00:49:34 +0100

    feat: add testing all pages

--- apps/philipplentzen/tests/index.spec.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from "@playwright/test";
-
-test(`landing page`, async ({ page }) => {
-  await page.goto(`/`);
-  await expect(page).toHaveScreenshot("index.png", { fullPage: true });
-});

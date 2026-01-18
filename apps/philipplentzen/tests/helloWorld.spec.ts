6bd6ad6dfa8e1cf791ecc0bb7462ea3cdd48dbd4
Philipp Lentzen <code@philipplentzen.dev>
2026-01-18 15:08:46 +0100

    feat: update test config

--- apps/philipplentzen/tests/helloWorld.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "@playwright/test";
-
-test(`homepage has "Hello World!" as content`, async ({ page }) => {
-  await page.goto(`/`);
-  const h1 = page.locator(`h1`);
-  await expect(h1).toHaveText(`Hello World!`);
-});

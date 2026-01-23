218b95668d6867d718d8b91a202fb0405fd2ae5f
Philipp Lentzen <code@philipplentzen.dev>
2026-01-23 23:55:05 +0100

    ci: upload playwright report artifact in tests workflow (#151)

--- apps/philipplentzen/components/today.tsx
+++ apps/philipplentzen/components/today.tsx
@@ -1,14 +1,17 @@
 "use client";
 
-import { type ComponentPropsWithoutRef, forwardRef } from "react";
+import {type ComponentPropsWithoutRef, forwardRef} from "react";
 
 export const Today = forwardRef<
   HTMLSpanElement,
   ComponentPropsWithoutRef<"span">
 >((props, ref) => {
+  console.log(process.env.NEXT_PUBLIC_IS_E2E);
   return (
     <span ref={ref} {...props}>
-      {new Date().toLocaleDateString()}
+      {process.env.NEXT_PUBLIC_IS_E2E === "1"
+        ? "E2E Mode"
+        : new Date().toLocaleDateString()}
     </span>
   );
 });
